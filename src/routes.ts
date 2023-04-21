2d0978d3c33c5d30f2a7a49ce4cc8fe6e1c050af
sasalx <coding@despanran.com>
2023-04-22 01:21:53 +0300

    feat: Routing Logic

---
+++ src/routes.ts
@@ -0,0 +1,13 @@
+import Home from "@/views/Home.vue";
+
+export const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  //   {
+  //     path: "/:catchAll(.*)*",
+  //     name: "NotFound",
+  //     component: PageNotFound,
+  //   },
+];
