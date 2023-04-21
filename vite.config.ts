0e31c82fc375efafd54839946a1500d2a92513ce
sasalx <coding@despanran.com>
2023-04-22 00:44:05 +0300

    feat: Add Alias Support for Imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     host: true,
   },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [vue()],
-})
+});
