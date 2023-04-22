edc2e2708e8ce8e5f2cedabbdbc6cae086ebed8d
Taylan Dogan <git@taylandogan.info>
2023-04-22 02:44:27 +0200

    feat: fetch anime from anilist graphql api
    Co-authored-by: sasalx <coding@despanran.com>
    
    Signed-off-by: Taylan Dogan <git@taylandogan.info>

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,44 @@
+<script lang="ts">
+import gql from "graphql-tag";
+
+const testQuery = gql`query ($id: Int) {
+   Media (id: $id, type: ANIME) {
+      id
+    title {
+      romaji
+    }
+  }
+}`
+
+</script>
+
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from "vue";
+import { useQuery } from '@vue/apollo-composable'
 
-defineProps<{ msg: string }>()
+const inputText = ref('')
 
-const count = ref(0)
+
+const show = ref(false)
+const queryInput = computed(() => { return { id: inputText.value ? inputText.value : 1 } })
+const { result: testResult, refetch } = useQuery(testQuery, queryInput)
+const testResultData = computed(() => testResult.value?.Media)
+
+
+
+// :')
+const buttoKun = () => {
+  refetch()
+  show.value = !show.value
+}
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+  <h1>Test Result</h1>
+  <input type="text" v-model="inputText" />
+  <button @click="buttoKun">Butto-kun</button>
+  <div v-if="show && testResultData">
+    <p>Data for {{ inputText }}</p>
+    <span>{{ testResultData.title.romaji }}</span>
   </div>
-
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
